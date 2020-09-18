9fb41c20b2b8a4cfd6f98a7d555fb9c24fd46b6d
TomarEmerald <xxmingexninjaxx@gmail.com>
2020-09-18 22:36:46 +0700

    first

--- src/components/Flight/Flights.jsx
+++ src/components/Flight/Flights.jsx
@@ -2,7 +2,7 @@ import Axios from "axios";
 import React, { useEffect, useState } from "react";
 import FlightOption from "./FlightOption";
 const Flights = () => {
-  const [flights, setFlights] = useState([{}, {}]);
+  const [flights, setFlights] = useState([]);
 
   const getFlights = async () => {
     Axios.get("/api/flight/").then((res) => setFlights(res.data));
