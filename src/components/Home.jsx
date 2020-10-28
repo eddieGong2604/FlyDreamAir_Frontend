18b4d4c8f90e37e936cc0262d8c76b3eb6668b81
eddieGong2604 <Illeniel18>
2020-10-28 11:49:51 +0700

    add image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,9 @@ import { Tabs } from "antd";
 import LoginComponent from "./Auth/LoginComponent";
 import SignUpComponent from "./Auth/SignUpComponent";
 import AdminSignup from "./AdminSignupComponent/AdminSignup";
+import BACKGROUND from "../img/back.jpg";
 
 const { TabPane } = Tabs;
-
 const callback = (key) => {};
 
 const tabStyle = {
@@ -20,7 +20,16 @@ const tabStyle = {
 
 const Home = () => {
   return (
-    <div>
+    <div style={{backgroundImage: `url(${BACKGROUND})` ,maxHeight: "100vh",
+
+    backgroundRepeat: "no-repeat",
+    backgroundAttachment: "fixed",
+    backgroundPosition: "left top",
+    backgroundSize: "cover"
+
+
+}}>
+      <div style={{width:"100vw" ,height: "100vh"}}></div>
       <Tabs defaultActiveKey="1" onChange={callback} style={tabStyle}>
         <TabPane tab="Log In" key="1">
           <LoginComponent />
